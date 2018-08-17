28f30302b52288d0b339941aa931061d176d0efd
d074177 <d074177@c02ww0bzhtd8.dhcp.muc2.sap.corp>
2018-08-17 15:34:42 +0200

    initial publishing...

---
+++ src/main/resources/data.sql
@@ -0,0 +1,41 @@
+insert into student
+values(10001,'Ranga', 'E1234567');
+
+insert into student
+values(10002,'Ravi', 'A1234568');
+
+insert into student
+values(10003,'Marcelo', 'A01234568');
+
+insert into student
+values(10004,'Karen', 'A02234568');
+
+insert into student
+values(10005,'Ken', 'A03234568');
+
+insert into student
+values(10006,'Josef', 'A0434568');
+
+insert into student
+values(10007,'Raul', 'A05234568');
+
+insert into student
+values(10008,'Richard', 'A0634568');
+
+insert into student
+values(10009,'Ronald', 'A07234568');
+
+insert into student
+values(10010,'Julio', 'A08234568');
+
+insert into student
+values(10011,'Maria', 'A09234568');
+
+insert into student
+values(10012,'Jose', 'A10234568');
+
+insert into student
+values(10013,'Maire', 'A111234568');
+
+insert into student
+values(10014,'Emily', 'A1234568');
